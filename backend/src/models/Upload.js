58145c951cbf2cd98800e76d5b86863ce5d02dac
arkodasgupta0412 <arkodasgupta05@gmail.com>
2026-02-26 12:37:28 +0530

    added upload schema

---
+++ backend/src/models/Upload.js
@@ -0,0 +1,30 @@
+import { Schema, model } from 'mongoose';
+
+const uploadSchema = new Schema({
+    uploader: { 
+        type: String, 
+        required: true 
+    }, 
+    
+    groupId: { 
+        type: Schema.Types.ObjectId, 
+        ref: 'Group', 
+        required: false 
+    },
+    
+    context: { 
+        type: String, 
+        enum: ['chat_media', 'group_avatar', 'user_avatar'], 
+        default: 'chat_media' 
+    },
+
+    filename: { type: String, required: true },
+    originalName: { type: String, required: true },
+    fileUrl: { type: String, required: true },
+    fileType: { type: String, enum: ['image', 'video', 'document', 'link'], default: 'image' },
+    mimeType: { type: String, required: true },
+    size: { type: Number, required: true }
+
+}, { timestamps: true });
+
+export default model('Upload', uploadSchema);
